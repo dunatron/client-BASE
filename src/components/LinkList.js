753163d85630f5f4209b32ff3b6b5fe6705a9ea7
Heath Dunlop <heath@silverstripe.com>
2018-09-05 01:21:40 +1200

    ff

--- src/components/LinkList.js
+++ src/components/LinkList.js
@@ -2,6 +2,8 @@ import React, { Component } from "react"
 import Link from "./Link"
 import { Query } from "react-apollo"
 import gql from "graphql-tag"
+import { AUTH_TOKEN } from "../constants"
+import { timeDifferenceForDate } from "../utils"
 
 // const FEED_QUERY = gql`
 //   {
@@ -22,16 +24,38 @@ import gql from "graphql-tag"
 //     }
 //   }
 // `
-const FEED_QUERY = gql`
-  query {
+export const FEED_QUERY = gql`
+  {
     feed {
-      id
-      description
+      links {
+        id
+        createdAt
+        url
+        description
+        postedBy {
+          id
+          name
+        }
+        votes {
+          id
+          user {
+            id
+          }
+        }
+      }
     }
   }
 `
 
 class LinkList extends Component {
+  _updateCacheAfterVote = (store, createVote, linkId) => {
+    const data = store.readQuery({ query: FEED_QUERY })
+
+    const votedLink = data.feed.links.find(link => link.id === linkId)
+    votedLink.votes = createVote.link.votes
+
+    store.writeQuery({ query: FEED_QUERY, data })
+  }
   render() {
     return (
       <Query query={FEED_QUERY}>
@@ -41,12 +65,17 @@ class LinkList extends Component {
 
           console.log(data)
 
-          const linksToRender = data.feed
+          const linksToRender = data.feed.links
 
           return (
             <div>
-              {linksToRender.map(link => (
-                <Link key={link.id} link={link} />
+              {linksToRender.map((link, index) => (
+                <Link
+                  key={link.id}
+                  link={link}
+                  index={index}
+                  updateStoreAfterVote={this._updateCacheAfterVote}
+                />
               ))}
             </div>
           )
